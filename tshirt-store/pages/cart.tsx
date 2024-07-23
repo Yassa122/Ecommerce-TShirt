4da551ec09d15ea2efdbdbd1841ae87d0060a3e6
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-23 20:04:55 +0300

    chore: Add Cart and Product page components for tshirt-store app

---
+++ tshirt-store/pages/cart.tsx
@@ -0,0 +1,24 @@
+import { useCart } from '../context/CartContext';
+
+const Cart: React.FC = () => {
+  const { cart } = useCart();
+
+  return (
+    <div className="container mx-auto px-4">
+      <h1 className="text-4xl font-bold my-8">Shopping Cart</h1>
+      {cart.length === 0 ? (
+        <p className="text-lg">Your cart is empty.</p>
+      ) : (
+        <ul>
+          {cart.map((item, index) => (
+            <li key={index} className="border p-4 my-2">
+              {item.name} - ${item.price}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Cart;
