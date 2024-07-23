b740e4c45637431c698da657f0e00ea8ead13770
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-23 20:04:46 +0300

    feat: Add CartContext for managing the shopping cart in tshirt-store app

---
+++ tshirt-store/context/CartContext.tsx
@@ -0,0 +1,36 @@
+import { createContext, ReactNode, useContext, useState } from 'react';
+
+interface CartItem {
+  id: string | string[];
+  name: string;
+  price: number;
+}
+
+interface CartContextType {
+  cart: CartItem[];
+  addToCart: (product: CartItem) => void;
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined);
+
+export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const [cart, setCart] = useState<CartItem[]>([]);
+
+  const addToCart = (product: CartItem) => {
+    setCart([...cart, product]);
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCart = (): CartContextType => {
+  const context = useContext(CartContext);
+  if (context === undefined) {
+    throw new Error('useCart must be used within a CartProvider');
+  }
+  return context;
+};
