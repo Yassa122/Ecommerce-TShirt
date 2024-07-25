e40c9c19cf016b30b0a7b629888d36890364a04c
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-25 16:45:30 +0300

    chore: Update adminRoutes to include getAllProducts endpoint

--- backend/src/routes/adminRoutes.ts
+++ backend/src/routes/adminRoutes.ts
@@ -1,11 +1,11 @@
 import express from 'express';
-import { addProduct } from '../controllers/adminController';
-import { authMiddleware } from '../middleware/authMiddleware';
-
+import { addProduct, getAllProducts } from '../controllers/adminController';
 const adminRoutes = express.Router();
 
 //Use adminAuthMiddleware to protect admin routes
-adminRoutes.post('/products', authMiddleware, addProduct);
+adminRoutes.post('/products',  addProduct);
+adminRoutes.get('/getAllProducts',  getAllProducts);
+
 // adminRoutes.get('/orders', adminAuthMiddleware, getOrders);
 // adminRoutes.post('/orders/respond', adminAuthMiddleware, respondToOrder);
 
