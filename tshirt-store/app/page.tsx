661a1abf273ec3f742e6410c93ff60f0db9e8dfb
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-26 01:36:46 +0300

    refactor: Update image container height to fill entire parent element

--- tshirt-store/app/page.tsx
+++ tshirt-store/app/page.tsx
@@ -55,7 +55,7 @@ const Home: React.FC = () => {
               whileHover={{ scale: 1.05 }}
             >
               <div className="w-full flex items-center justify-center" style={{ height: '300px' }}>
-                <img src={product.Images[0]} alt={product.ProductName} style={{ objectFit: 'cover' }} className="object-contain w-full h-auto" />
+                <img src={product.Images[0]} alt={product.ProductName} style={{ objectFit: 'cover' }} className="object-contain w-full h-full" />
               </div>
               <motion.div
                 className="absolute inset-0 bg-black bg-opacity-75 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center p-4"
