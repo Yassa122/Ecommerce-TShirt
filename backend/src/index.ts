ca5650274225b2e1e4ec96acf7ddbc6b1656d9d6
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-25 16:46:40 +0300

    chore: Update CORS configuration for backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,22 +1,27 @@
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import './firebase'; // Ensure Firebase is initialized before anything else
 import { db } from './firebase';
+import adminRoutes from './routes/adminRoutes'; // Import admin routes
 import userRoutes from './routes/userRoutes';
-
 dotenv.config();
 console.log('Environment variables loaded');
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
+
 const port = process.env.PORT || 3000;
 
 console.log('Registering routes');
-app.use('/api/users', userRoutes);
+app.use(cors({
+  origin: 'http://localhost:3001', // Allow requests from this origin
+  methods: 'GET,POST', // Allow only these methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow only these headers
+}));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+app.use('/api/users', userRoutes);
+app.use('/api/admin', adminRoutes); // Register admin routes
 
 app.get('/testFirestore', async (req, res) => {
   try {
@@ -31,4 +36,8 @@ app.get('/testFirestore', async (req, res) => {
     console.error('Error accessing Firestore:', error);
     res.status(500).send((error as Error).message);
   }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
