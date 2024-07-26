c128a2e27341f417727e6f8789386ac7134cbc42
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-27 01:49:11 +0300

    feat: Add lazy loading for images in home page

--- tshirt-store/app/page.tsx
+++ tshirt-store/app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import axios from "axios";
 import { motion } from "framer-motion";
+import Image from "next/image"; // Import Image component
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import Carousel from "../components/carousel";
@@ -9,14 +10,12 @@ import Navbar from "../components/navbar";
 const Home: React.FC = () => {
   const [products, setProducts] = useState([]);
   const [galleryImages, setGalleryImages] = useState([
-    // Add your gallery images URLs here
     "https://via.placeholder.com/800x400", // Example image
     "https://via.placeholder.com/800x400", // Example image
     "https://via.placeholder.com/800x400", // Example image
   ]);
 
   useEffect(() => {
-    // Fetch actual products from your API
     axios.get("http://localhost:3000/api/users/products")
       .then((response) => {
         setProducts(response.data);
@@ -57,11 +56,17 @@ const Home: React.FC = () => {
           {products.map((product) => (
             <motion.div
               key={product.id}
-              className="relative bg-zinc-900 dark:bg-white rounded-lg overflow-hidden shadow-2xl hover:shadow-2xl transition-shadow duration-300 drop-shadow-2xl"
+              className="relative bg-zinc-900 dark:bg-white rounded-lg overflow-hidden shadow-2xl hover:shadow-2xl transition-shadow duration-300"
               whileHover={{ scale: 1.05 }}
             >
               <div className="w-full flex items-center justify-center" style={{ height: '300px' }}>
-                <img src={product.Images[0]} alt={product.ProductName} style={{ objectFit: 'cover' }} className="object-contain w-full h-full" />
+                <Image
+                  src={product.Images[0]}
+                  alt={product.ProductName}
+                  layout="fill"
+                  objectFit="cover"
+                  className="object-contain w-full h-full"
+                />
               </div>
               <motion.div
                 className="absolute inset-0 bg-black bg-opacity-75 opacity-0 hover:opacity-100 transition-opacity duration-300 flex items-center justify-center p-4"
