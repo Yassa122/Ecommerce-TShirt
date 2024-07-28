43d669ed001e102d1770747c096fd537db6e19b6
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-29 02:21:18 +0300

    feat: Update PWA manifest.json with new app name, icon paths, and metadata

--- tshirt-store/app/pages/_document.tsx
+++ tshirt-store/app/pages/_document.tsx
@@ -1,14 +1,24 @@
 // pages/_document.tsx
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-          <meta name="theme-color" content="#000000" />
+          <meta name="theme-color" content="#2196f3" />
+          <meta name="description" content="PWA application with Next 13" />
+          <meta name="keywords" content="nextjs, nextjs13, next13, pwa, next-pwa" />
+          <link rel="apple-touch-icon" href="/icons/icon-128x128.png" />
+          <link rel="icon" href="/icons/icon-128x128.png" />
+          {/* Additional global styles or fonts */}
         </Head>
         <body>
           <Main />
