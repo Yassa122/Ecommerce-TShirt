dd804bd6c2073b3e2faf19d03c7b83a3dc5f6d2f
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-31 00:09:27 +0300

    testing backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser()); // Parse cookies
 const port = process.env.PORT || 3000;
 
 // Get CORS origin from environment variable
-const corsOrigin = "https://amaria-local.vercel.app";
+const corsOrigin = "http://localhost:3001";
 
 console.log('Registering routes');
 app.use(cors({
