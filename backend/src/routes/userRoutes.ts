b59f409958dd30d24b5d9eb1ca82b5e2dce9a519
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-31 16:57:22 +0300

    feat: Add checkout functionality to userRoutes
    The code changes in userRoutes add the '/checkout' endpoint to handle the checkout process. This allows users to complete their purchase by sending a POST request to '/checkout'. The checkout functionality is implemented in the checkout controller. This change enhances the user experience by providing a seamless checkout process.

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addToCart, getProductById, getProducts } from '../controllers/userController';
+import { addToCart, checkout, getProductById, getProducts } from '../controllers/userController';
 
 const userRoutes = express.Router();
 
@@ -8,5 +8,6 @@ console.log('Registering /products route');
 userRoutes.get('/products', getProducts);
 userRoutes.get('/products/:id', getProductById);
 userRoutes.post('/cart', addToCart);
+userRoutes.post('/checkout', checkout);
 
 export default userRoutes;
