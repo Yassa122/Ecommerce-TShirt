253762169731bff1e83181d2ded61627591555d7
Yassa122 <yassa.ashraf@yahoo.com>
2024-07-31 17:14:26 +0300

    chore: Update CORS origin to use production frontend URL

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser()); // Parse cookies
 const port = process.env.PORT || 3000;
 
 // Get CORS origin from environment variable
-const corsOrigin = "http://localhost:3001";
+const corsOrigin = "amaria-local.vercel.app";
 
 console.log('Registering routes');
 app.use(cors({
