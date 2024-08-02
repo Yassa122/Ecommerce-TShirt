0a7fd189807df99bc361d6a183095c0bd15f5f21
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-02 14:46:57 +0300

    refactor: Update firebase messaging service worker

--- tshirt-store/app/config/firebaseConfig.ts
+++ tshirt-store/app/config/firebaseConfig.ts
@@ -1,6 +1,5 @@
-// firebaseConfig.ts
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+import { initializeApp } from "firebase/app";
+import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7fjT3BJOxTA6dHSFH4PyrCtaMpK5OAWs",
@@ -11,11 +10,8 @@ const firebaseConfig = {
   appId: "1:580207836879:web:d52d32c83768a9b67741ba",
 };
 
-// Initialize Firebase app
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-
-// Initialize Firebase Cloud Messaging and get a reference to the service
+const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
-export { app, getToken, messaging, onMessage };
+export { getToken, messaging, onMessage };
 
