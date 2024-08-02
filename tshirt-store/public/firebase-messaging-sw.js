a6aa698eb1d887d938f445daae0bad8d19709b17
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-02 14:42:40 +0300

    refactor: Update firebase messaging service worker
    The code changes update the firebase-messaging-sw.js file to ensure compatibility with the latest version of Firebase Cloud Messaging. This includes importing the necessary Firebase modules and initializing the Firebase app. The service worker script now handles background messages and displays notifications when a background message is received, enabling push notifications for the application.

--- tshirt-store/public/firebase-messaging-sw.js
+++ tshirt-store/public/firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/9.6.7/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.6.7/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-    apiKey: "AIzaSyB7fjT3BJOxTA6dHSFH4PyrCtaMpK5OAWs",
-    authDomain: "e-commerce-t-shirt.firebaseapp.com",
-    projectId: "e-commerce-t-shirt",
-    storageBucket: "e-commerce-t-shirt.appspot.com",
-    messagingSenderId: "580207836879",
-    appId: "1:580207836879:web:d52d32c83768a9b67741ba",
+  apiKey: "AIzaSyB7fjT3BJOxTA6dHSFH4PyrCtaMpK5OAWs",
+  authDomain: "e-commerce-t-shirt.firebaseapp.com",
+  projectId: "e-commerce-t-shirt",
+  storageBucket: "e-commerce-t-shirt.appspot.com",
+  messagingSenderId: "580207836879",
+  appId: "1:580207836879:web:d52d32c83768a9b67741ba",
 });
 
 const messaging = firebase.messaging();
