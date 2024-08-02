4e4b88499648173a527405100edb96d7c307697a
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-02 16:22:14 +0300

    refactor: Update Firebase messaging service worker and messaging service for compatibility with latest version of Firebase Cloud Messaging

--- tshirt-store/app/config/firebaseConfig.ts
+++ tshirt-store/app/config/firebaseConfig.ts
@@ -1,6 +1,5 @@
-// firebaseConfig.ts
-import { FirebaseApp, getApps, initializeApp } from 'firebase/app';
-import { getMessaging, getToken, Messaging, onMessage } from 'firebase/messaging';
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 const firebaseConfig = {
   apiKey: "AIzaSyB7fjT3BJOxTA6dHSFH4PyrCtaMpK5OAWs",
@@ -11,11 +10,44 @@ const firebaseConfig = {
   appId: "1:580207836879:web:d52d32c83768a9b67741ba",
 };
 
-let messaging: Messaging | undefined;
-if (typeof window !== "undefined" && !getApps().length) {
-  const app: FirebaseApp = initializeApp(firebaseConfig);
-  messaging = getMessaging(app);
-}
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
 
-export { getToken, messaging, onMessage };
+export const requestNotificationPermission = async () => {
+  try {
+    const currentToken = await getToken(messaging, { vapidKey: 'YOUR_VAPID_KEY' });
+    if (currentToken) {
+      console.log('FCM Token:', currentToken);
+      // Send the token to the server and store it
+      await fetch('https://your-server-url.com/save-token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ token: currentToken }),
+      });
 
+      // Subscribe to the topic
+      await fetch(`https://iid.googleapis.com/iid/v1/${currentToken}/rel/topics/admin-notifications`, {
+        method: 'POST',
+        headers: new Headers({
+          'Authorization': 'key=YOUR_SERVER_KEY'
+        })
+      });
+
+      console.log('Subscribed to admin-notifications topic.');
+    } else {
+      console.log('No registration token available. Request permission to generate one.');
+    }
+  } catch (err) {
+    console.error('An error occurred while retrieving token. ', err);
+  }
+};
+
+onMessage(messaging, (payload) => {
+  console.log('Message received. ', payload);
+  // Customize your notification here
+  alert(payload.notification?.title);
+});
+
+export { messaging };
