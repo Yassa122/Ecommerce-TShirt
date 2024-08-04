46deb7a976105594ff2d94d784a0721d13bfa869
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-04 18:02:10 +0300

    refactor: Update service worker to improve compatibility with Firebase Cloud Messaging and optimize performance

--- tshirt-store/app/pages/adminHome/page.tsx
+++ tshirt-store/app/pages/adminHome/page.tsx
@@ -12,16 +12,13 @@ import {
   Title,
   Tooltip,
 } from "chart.js";
-import { onMessage } from "firebase/messaging";
 import { motion } from "framer-motion";
 import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Doughnut, Line } from "react-chartjs-2";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import { HiMenuAlt1 } from "react-icons/hi"; // Importing the icon for the sidebar toggle button
-import { messaging, requestNotificationPermission } from "../../config/firebaseConfig";
 import AddProduct from "../addProduct/page";
-import SubscribeToNotifications from "./SubscribeToNotifications";
 
 ChartJS.register(
   CategoryScale,
@@ -50,7 +47,7 @@ interface Order {
   netWorth: number;
 }
 
-const AdminHome: React.FC = () => {
+const AdminHome = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
   const [orders, setOrders] = useState<Order[]>([]);
@@ -58,7 +55,6 @@ const AdminHome: React.FC = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [editing, setEditing] = useState(false);
-  const [editedProduct, setEditedProduct] = useState<Product | null>(null);
   const router = useRouter();
 
   useEffect(() => {
@@ -78,16 +74,6 @@ const AdminHome: React.FC = () => {
         netWorth: order.TotalPrice - (order.deliveryFee || 0)
       }));
       setOrders(transformedOrders);
-    });
-
-    // Request permission for notifications
-    requestNotificationPermission();
-
-    // Handle incoming messages
-    onMessage(messaging, (payload) => {
-      console.log('Message received. ', payload);
-      // Customize your notification here
-      alert(payload.notification?.title);
     });
   }, []);
 
@@ -162,47 +148,25 @@ const AdminHome: React.FC = () => {
     }
   };
 
-  const handleCardClick = (product: Product) => {
+  const handleCardClick = (id: string) => {
     if (!editing) {
-      setEditedProduct(product);
-      setEditing(true);
+      router.push(`/pages/editProduct/${id}`);
     }
   };
 
-  const handleTextChange = (field: keyof Product, value: string | number) => {
-    if (editedProduct) {
-      setEditedProduct({ ...editedProduct, [field]: value });
-    }
+  const handleTextChange = (id: string, field: keyof Product, value: string | number) => {
+    setProducts(products.map(product =>
+      product.id === id ? { ...product, [field]: value } : product
+    ));
   };
 
-  const saveChanges = async () => {
-    if (editedProduct) {
-      try {
-        await axios.put(`https://amaria-backend.vercel.app/api/admin/editProduct/${editedProduct.id}`, editedProduct);
-        setProducts(products.map(product => product.id === editedProduct.id ? editedProduct : product));
-        setFilteredProducts(filteredProducts.map(product => product.id === editedProduct.id ? editedProduct : product));
-        setEditing(false);
-        setEditedProduct(null);
-        alert("Product updated successfully!");
-      } catch (error) {
-        console.error("Error updating product:", error);
-        alert("Failed to update product. Please try again.");
-      }
-    }
-  };
-
-  const cancelEditing = () => {
-    setEditing(false);
-    setEditedProduct(null);
+  const toggleEditing = () => {
+    setEditing(!editing);
   };
 
   const toggleSidebar = () => {
     setSidebarOpen(!sidebarOpen);
   };
-
-  function toggleEditing() {
-    throw new Error("Function not implemented.");
-  }
 
   return (
     <div className={`${darkMode ? "dark" : ""} flex min-h-screen`}>
@@ -220,169 +184,98 @@ const AdminHome: React.FC = () => {
           <HiMenuAlt1 size={24} />
         </button>
       </div>
-      <SubscribeToNotifications /> {/* Add this line */}
 
-      <main className="flex-1 p-4 md:p-8 bg-gray-100 dark:bg-black transition-all duration-300 ease-in-out">
+      <main className="flex-1 p-8 bg-gray-100 dark:bg-black transition-all duration-300 ease-in-out">
         <div className="flex justify-between items-center mb-8">
-          <h1 className="text-2xl md:text-3xl font-semibold text-gray-700 dark:text-gray-300">Dashboard</h1>
+          <h1 className="text-3xl font-semibold text-gray-700 dark:text-gray-300">Dashboard</h1>
           <div className="w-full md:w-1/3">
             <input
               type="text"
               placeholder="Search products..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring focus:ring-blue-200 dark:focus:ring-blue-800 text-sm md:text-base"
+              className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring focus:ring-blue-200 dark:focus:ring-blue-800"
             />
           </div>
         </div>
-        {editing && editedProduct ? (
-          <div className="bg-white dark:bg-zinc-900 p-4 md:p-6 rounded-lg shadow-lg">
-            <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Edit Product</h2>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300 mb-2">Product Name</label>
-              <input
-                type="text"
-                value={editedProduct.ProductName}
-                onChange={(e) => handleTextChange('ProductName', e.target.value)}
-                className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300 mb-2">Type</label>
-              <input
-                type="text"
-                value={editedProduct.Type}
-                onChange={(e) => handleTextChange('Type', e.target.value)}
-                className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300 mb-2">Price</label>
-              <input
-                type="number"
-                value={editedProduct.Price}
-                onChange={(e) => handleTextChange('Price', e.target.value)}
-                className="w-full p-2 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none"
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-gray-700 dark:text-gray-300 mb-2">Sizes</label>
-              <ul className="list-disc pl-5">
-                {editedProduct.Sizes.map((sizeObj, index) => (
-                  <li key={index} className="mt-1">
-                    <span className="font-medium">Size:</span> 
-                    <input
-                      type="text"
-                      value={sizeObj.size}
-                      onChange={(e) => handleTextChange(`Sizes.${index}.size` as keyof Product, e.target.value)}
-                      className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
-                    />
-                    <span className="font-medium"> Quantity:</span>
-                    <input
-                      type="number"
-                      value={sizeObj.quantity}
-                      onChange={(e) => handleTextChange(`Sizes.${index}.quantity` as keyof Product, e.target.value)}
-                      className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="flex justify-end space-x-4">
-              <button
-                className="text-blue-500 hover:underline font-bold"
-                onClick={saveChanges}
-              >
-                Save
-              </button>
-              <button
-                className="text-red-500 hover:underline font-bold"
-                onClick={cancelEditing}
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
-            {filteredProducts.map((product) => (
-              <motion.div
-                key={product.id}
-                className="bg-white dark:bg-zinc-900 p-4 md:p-6 rounded-lg shadow-lg cursor-pointer"
-                initial={{ opacity: 0, y: 10 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5 }}
-                whileHover={{ scale: 1.02 }}
-                onClick={() => handleCardClick(product)}
-              >
-                <img src={product.Images[0]} alt={product.ProductName} className="mb-4 max-h-40 w-full object-cover rounded-md" />
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    value={product.ProductName}
-                    onChange={(e) => handleTextChange('ProductName', e.target.value)}
-                    className="bg-transparent w-full text-lg md:text-xl font-semibold text-gray-700 dark:text-gray-300 border-b border-transparent focus:outline-none focus:border-blue-500"
-                    disabled={!editing}
-                  />
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="text"
-                    value={product.Type}
-                    onChange={(e) => handleTextChange('Type', e.target.value)}
-                    className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
-                    disabled={!editing}
-                  />
-                </div>
-                <div className="mb-2">
-                  <input
-                    type="number"
-                    value={product.Price}
-                    onChange={(e) => handleTextChange('Price', e.target.value)}
-                    className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
-                    disabled={!editing}
-                  />
-                </div>
-                <div className="text-gray-600 dark:text-gray-400">
-                  <p className="font-medium">Sizes and Quantities:</p>
-                  <ul className="list-disc pl-5">
-                    {product.Sizes.map((sizeObj, index) => (
-                      <li key={index} className="mt-1">
-                        <span className="font-medium">Size:</span> {sizeObj.size}, 
-                        <span className="font-medium"> Quantity:</span> {sizeObj.quantity}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-4 flex justify-end space-x-4">
-                  <button
-                    className="text-blue-500 hover:underline font-bold"
-                    onClick={(e) => { e.stopPropagation(); toggleEditing(); }}
-                  >
-                    <FaEdit className="inline-block mr-2" />
-                    {editing ? 'Save' : 'Edit'}
-                  </button>
-                  <button
-                    className="text-red-500 hover:underline font-bold"
-                    onClick={(e) => { e.stopPropagation(); handleDelete(product.id); }}
-                  >
-                    <FaTrash className="inline-block mr-2" />
-                    Delete
-                  </button>
-                </div>
-              </motion.div>
-            ))}
-          </div>
-        )}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {filteredProducts.map((product) => (
+            <motion.div
+              key={product.id}
+              className="bg-white dark:bg-zinc-900 p-6 rounded-lg shadow-lg cursor-pointer"
+              initial={{ opacity: 0, y: 10 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5 }}
+              whileHover={{ scale: 1.02 }}
+              onClick={() => handleCardClick(product.id)}
+            >
+              <img src={product.Images[0]} alt={product.ProductName} className="mb-4 max-h-40 w-full object-cover rounded-md" />
+              <div className="mb-2">
+                <input
+                  type="text"
+                  value={product.ProductName}
+                  onChange={(e) => handleTextChange(product.id, 'ProductName', e.target.value)}
+                  className="bg-transparent w-full text-lg font-semibold text-gray-700 dark:text-gray-300 border-b border-transparent focus:outline-none focus:border-blue-500"
+                  disabled={!editing}
+                />
+              </div>
+              <div className="mb-2">
+                <input
+                  type="text"
+                  value={product.Type}
+                  onChange={(e) => handleTextChange(product.id, 'Type', e.target.value)}
+                  className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
+                  disabled={!editing}
+                />
+              </div>
+              <div className="mb-2">
+                <input
+                  type="number"
+                  value={product.Price}
+                  onChange={(e) => handleTextChange(product.id, 'Price', e.target.value)}
+                  className="bg-transparent w-full text-gray-600 dark:text-gray-400 border-b border-transparent focus:outline-none focus:border-blue-500"
+                  disabled={!editing}
+                />
+              </div>
+              <div className="text-gray-600 dark:text-gray-400">
+                <p className="font-medium">Sizes and Quantities:</p>
+                <ul className="list-disc pl-5">
+                  {product.Sizes && product.Sizes.map((sizeObj, index) => (
+                    <li key={index} className="mt-1">
+                      <span className="font-medium">Size:</span> {sizeObj.size}, 
+                      <span className="font-medium"> Quantity:</span> {sizeObj.quantity}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="mt-4 flex justify-end space-x-4">
+                <button
+                  className="text-blue-500 hover:underline font-bold"
+                  onClick={(e) => { e.stopPropagation(); toggleEditing(); }}
+                >
+                  <FaEdit className="inline-block mr-2" />
+                  {editing ? 'Save' : 'Edit'}
+                </button>
+                <button
+                  className="text-red-500 hover:underline font-bold"
+                  onClick={(e) => { e.stopPropagation(); handleDelete(product.id); }}
+                >
+                  <FaTrash className="inline-block mr-2" />
+                  Delete
+                </button>
+              </div>
+            </motion.div>
+          ))}
+        </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-8">
           <motion.section
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="bg-white dark:bg-zinc-900 p-4 md:p-6 rounded-lg shadow-lg"
+            className="bg-white dark:bg-zinc-900 p-6 rounded-lg shadow-lg"
           >
-            <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Add New Product</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Add New Product</h2>
             <AddProduct />
           </motion.section>
 
@@ -390,9 +283,9 @@ const AdminHome: React.FC = () => {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="bg-white dark:bg-zinc-900 p-4 md:p-6 rounded-lg shadow-lg"
+            className="bg-white dark:bg-zinc-900 p-6 rounded-lg shadow-lg"
           >
-            <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Order Summary</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Order Summary</h2>
             <Line data={orderData} />
           </motion.section>
 
@@ -400,9 +293,9 @@ const AdminHome: React.FC = () => {
             initial={{ opacity: 0, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
-            className="bg-white dark:bg-zinc-900 p-4 md:p-6 rounded-lg shadow-lg"
+            className="bg-white dark:bg-zinc-900 p-6 rounded-lg shadow-lg"
           >
-            <h2 className="text-xl md:text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Financial Overview</h2>
+            <h2 className="text-2xl font-semibold mb-4 text-gray-700 dark:text-gray-300">Financial Overview</h2>
             <Doughnut data={donutData} />
           </motion.section>
         </div>
