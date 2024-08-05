0775c77f1b919315eec81136c3baca6fbb8b6306
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-05 20:29:17 +0300

    refactor: Reorder imports in authRoutes.ts and adminController.ts

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, signin } from '../controllers/authController';
+import { signin, signup } from '../controllers/authController';
 
 const router = express.Router();
 
