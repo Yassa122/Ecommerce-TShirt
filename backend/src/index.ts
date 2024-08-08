0a876e4b7fbcd96a356151a301f9debf14e6a3d0
Yassa122 <yassa.ashraf@yahoo.com>
2024-08-08 19:06:57 +0300

    chore: Update CORS origin handling in index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -18,7 +18,7 @@ app.use(cookieParser()); // Parse cookies
 const port = process.env.PORT || 3000;
 
 // Get CORS origin from environment variable
-const corsOrigin = "https://amaria-local.vercel.app"; // Use the CORS origin from environment variables
+const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:3001"; // Use the CORS origin from environment variables
 console.log('Registering routes');
 app.use(cors({
   origin: corsOrigin, // Use the CORS origin from environment variables
